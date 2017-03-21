3725823ece099d87eba703cfad466db7f802bcb9
Liam Davenport <liam.davenport@jobsite.co.uk>
2017-03-21 08:30:50 +0000

    feature/prettier - prettier

--- src/index.js
+++ src/index.js
@@ -8,8 +8,6 @@ const { host, port } = config.get('server');
 routes.register(app);
 
 app.listen(port, () => {
-  console.log(`Front-End server is running at http://${host}:${port}`);
+  console.log(`✅ 😀 - Front-End server is running at http://${host}:${port}`);
   console.log(`Try me: http://${host}:${port}/v1/someapi/promise`);
 });
-
-
