6f4a2092272484666d8ec9e212fce041b9005486
Liam Davenport <liam.davenport@jobsite.co.uk>
2017-04-27 11:24:02 +0100

    feature/upgrade - upgrade packages and make all run with yarn

--- bin/start.sh
+++ bin/start.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
-nvmpath=/opt/jsnode
-source "${nvmpath}/nvm.sh"
 if [ "$1" == 'test' ]
 then
-	npm test
+	yarn test
 elif  [ "$1" == 'test:coverage' ] #build test documentation
 then
-	npm run test:coverage
+	yarn test:coverage
 elif  [ "$1" == 'build' ] #build compiled babel version
 then
-	npm run build
+	yarn build
 elif  [ "$1" == 'start' ] #start from compiled babel version
 then
-	npm start
+	yarn start
 else #dev mode - run using nodemon and babel-node
 	nvm use
 	yarn install
-	npm run start:dev
+	yarn start:dev
 	node --version
 fi
